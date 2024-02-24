a019523dd9ba1e97a9e9ab872738d17aa1358e99
arildo <arildohp@gmail.com>
2024-02-24 09:56:28 -0300

    projeto finalizado

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { Game } from '../App'
 const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:3000'
+    baseUrl: 'http://localhost:4000'
   }),
   endpoints: (builder) => ({
     getJogos: builder.query<Game[], void>({
